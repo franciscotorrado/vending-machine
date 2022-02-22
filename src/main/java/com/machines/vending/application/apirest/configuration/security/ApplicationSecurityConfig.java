b7567fa2333bc9b99b5b8ce64fda0bb3050561ff
Paco Torrado <developer@pacotorrado.com>
2022-02-22 02:57:19 +0100

    Create constructor

--- src/main/java/com/machines/vending/application/apirest/configuration/security/ApplicationSecurityConfig.java
+++ src/main/java/com/machines/vending/application/apirest/configuration/security/ApplicationSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.machines.vending.application.apirest.configuration.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,8 +11,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    public ApplicationSecurityConfig(final AuthenticationEntryPoint authenticationEntryPoint) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
